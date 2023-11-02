b6d6a280479a16bb6672cced31ca388e229aa046
jash1811 <22dcs042@charusat.edu.in>
2023-11-02 21:01:02 +0530

    design change in signin and signup page

---
+++ index.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const Window = require('window');
+const DB = require(__dirname + "/routes/db.js");
+port = 3000;
+
+app = express();
+const window = new Window();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("views/CSS"));
+app.use(express.static("views/templates"));
+
+
+app.get('/', function(req, res) {
+    res.render("templates/home");
+});
+
+app.get('/signup1', function(req, res) {
+    res.render("templates/signup1");
+});
+
+
+
+app.get('/home', function(req, res) {
+    res.render("templates/home");
+});
+
+app.get('/signin', function(req, res) {
+    res.render("templates/signin");
+});
+
+app.get('/signup', function(req, res) {
+    res.render("templates/signup");
+});
+
+app.post('/signup', function(req, res) {
+    console.log(req.body);
+    //DB.signup(req.body);
+    res.redirect("/signin");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+});
