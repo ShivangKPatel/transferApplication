95f6c8d4980033702b5095ab67b79c13cd718db7
Shivangkpatel <Shivang02052005@gmail.com>
2023-11-03 20:59:31 +0530

    navigation bar updated with website logo

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(express.static("public"));
 app.use(express.static("views/CSS"));
 app.use(express.static("views/templates"));
 
+//VerificationNumber
+var verificationnum = 0;
+let careateAccountData;
 
 app.get('/', function(req, res) {
     res.render("templates/home");
@@ -22,17 +25,61 @@ app.get('/home', function(req, res) {
 });
 
 app.get('/signin', function(req, res) {
-    res.render("templates/signin");
+    res.render("templates/signin", { flag: 0 }) //flag=0 means no error
+})
+
+app.post('/signin', async function(req, res) {
+    checkData = req.body;
+    console.log(checkData);
+    logedInUserData = await DB.isLogin(checkData);
+    if (logedInUserData.length != 0) {
+        logedInUserData = logedInUserData[0];
+        console.log(logedInUserData);
+        if (logedInUserData.userpass == checkData.password) {
+            DB.logData(logedInUserData.useremail, logedInUserData.userid);
+            res.render("templates/home")
+        } else {
+            res.render("templates/signin", { flag: 2 }) //flag=2 means password is wrong
+        }
+    } else {
+        res.render("templates/signin", { flag: 1 }) //flag=1 means account not found
+    }
 });
 
 app.get('/signup', function(req, res) {
-    res.render("templates/signup");
+    res.render("templates/signup", { flag: 0 }); //flag=0 means no error
 });
 
-app.post('/signup', function(req, res) {
-    console.log(req.body);
-    //DB.signup(req.body);
-    res.redirect("/signin");
+app.post('/signup', async function(req, res) {
+    createAccountData = req.body;
+    console.log(createAccountData);
+    verificationnum = await DB.isFound(createAccountData.email);
+    console.log(verificationnum);
+    if (verificationnum == 0) {
+        res.render("templates/signup", { flag: 1 }); //flag=1 means account already exist
+    } else {
+        const message = await DB.OTPMessage(createAccountData.firstname, verificationnum);
+        try {
+            await DB.sendEmail(createAccountData.email, "Verify your self on bookMyCelebration...", message);
+        } catch (error) {
+            console.log("Error got in sending an email");
+        }
+        res.render("templates/verification", { flag: 0, mail: createAccountData.email }); //flag=0 means no error
+    }
+});
+
+app.get('/verification', function(req, res) {
+    res.render("templates/verification", { flag: 0, mail: createAccountData.email });
+});
+
+app.post('/verification', function(req, res) {
+    numbrerGot = req.body;
+    if (verificationnum == numbrerGot.OTPVerification) {
+        DB.createNewAccount(createAccountData);
+        res.render("templates/signin", { flag: 3 }) //flag=3 means account created successfully and login using your email and password
+    } else {
+        res.render("templates/verification", { flag: 1, log: createAccountData }); //flag=1 means OTP is wrong
+    }
 });
 
 app.listen(port, () => {
--- routes/db.js
+++ routes/db.js
@@ -1,1 +1,125 @@
-    
+var mysql2 = require('mysql2');
+var nodemailer = require('nodemailer');
+
+const connection = mysql2.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'transferapplication',
+    port: '3308'
+}).promise();
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        //lavamwbdgvqvemup
+        user: 'smart20072020@gmail.com',
+        pass: 'lavamwbdgvqvemup'
+    }
+});
+
+// Text send to encrypt function
+async function onStart() {
+    var hw = encrypt("shivang02052005@gmail.com")
+    res = await connection.query(`select * from encription`);
+    res = res[0][0];
+    console.log(res[0]);
+    console.log(hw)
+    hw = res;
+    console.log(decrypt(hw))
+}
+
+
+async function isFound(emailCA) {
+    var min = 1000;
+    var max = 9999;
+    var num = Math.floor(Math.random() * (max - min + 1)) + min;
+    const [result] = await connection.query(`select * from master where useremail="${emailCA}"`);
+    if (result.length != 0) {
+        num = 0;
+    }
+    return num;
+}
+
+async function isLogin({ username, email, password, role }) {
+    const [result] = await connection.query(`select * from master where useremail="${email}" AND userpass="${password}"`);
+    return result;
+}
+
+function feedBack({ email, subject, feedbacktext }) {
+    connection.query(`insert into feedback (email, subject, message) values ("${email}","${subject}","${feedbacktext}")`)
+    return;
+}
+
+function logData(email, userid) {
+    var datetime = new Date();
+    date = datetime.toISOString().slice(0, 10);
+    const hours = datetime.getHours();
+    const minutes = datetime.getMinutes();
+    const time = `${hours}:${minutes}`;
+    connection.query(`insert into log (userid ,email, date,time) value ("${userid}" , "${email}","${date}","${time}")`);
+}
+
+function sendEmail(mailAddress, subject, arghtml) {
+    var mailOptions = {
+        from: 'smart20072020@gmail.com',
+        to: `${mailAddress}`,
+        subject: `${subject}`,
+        html: `${arghtml}`
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) throw error;
+        return;
+    });
+}
+
+function OTPMessage(firstname, number) {
+    const message = `
+    <head>
+        <link rel="stylesheet" href="CSS/stylemail.css">
+    </head>
+    <table id="m_8636265828324098607udemy-email" width="100%" cellpadding="0" cellspacing="0" style="background-color:#f7f9fa;padding:24px">
+        <tbody><tr>
+            <td>&nbsp;</td>
+            <td width="600">
+                <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#fff">
+                        <tbody><tr>
+                            <td style="border-bottom:1px solid #cccccc;padding:24px">
+                                <h2>Student college transfer application</h2>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 15px 24px 0 24px">
+                                <p><a style="text-decoration:none;color:#1c1d1f">
+                                    Hi ${firstname}
+                                </a></p>
+                                <p>
+                                    <a style="text-decoration:none;color:#1c1d1f"><b></b></a><b><a style="text-decoration:none;color:#1c1d1f">Creating a new account</a></b>
+                                </p>
+                                <p>
+                                    <a style="text-decoration:none;color:#1c1d1f">Your 4-Digit verification code is <b style="font-size: 16px; margin-top: 4px; margin-left: 4px;">${number}</b></a>
+                                </p>
+                                <p>
+                                    <a style="text-decoration:none;color:#1c1d1f">This code was sent to you to verify your new account. </a>
+                                </p>
+                                <p style="margin-bottom:0">
+                                    <a style="text-decoration:none;color:#1c1d1f">If you didn't request a code then ignore it.</a>
+                                </p>   
+                            </td>
+                        </tr>
+                    <tr>
+                        <td style="padding:24px 0 0 0"></td>
+                    </tr>
+                </tbody></table>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </tbody></table>`;
+    return message;
+}
+
+function createNewAccount({ firstname, middlename, lastname, email, password, contactno, select }) {
+    connection.query(`insert into master(firstname, middlename, lastname, useremail, userpass, contactno, usertype) values("${firstname}", "${middlename}", "${lastname}", "${email}", "${password}", "${contactno}", "${select}")`);
+}
+
+module.exports = { isFound, sendEmail, OTPMessage, createNewAccount, isLogin, logData, feedBack };
