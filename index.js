88094a211ee24a12c1e21094c1c3dca3ef7bcb94
jash1811 <22dcs042@charusat.edu.in>
2023-11-04 01:28:49 +0530

    adding nave pannel

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const bodyParser = require('body-parser');
-const { response } = require('express');
 const express = require('express');
 const Window = require('window');
 const DB = require(__dirname + "/routes/db.js");
@@ -13,38 +12,55 @@ app.use(express.static("public"));
 app.use(express.static("views/CSS"));
 app.use(express.static("views/templates"));
 
+//global variables
+let verificationNum = 0;
+let createAccountData;
+let logedInUserData;
+let LoginData;
+let venueList;
+let upcomingEventList;
+let flag = 0;
 
-app.get('/', function(req, res) {
+//Routes for rendaring the file
+app.get('/', function (req, res) {
     res.render("templates/home");
 });
 
-app.get('/signup1', function(req, res) {
-    res.render("templates/signup1");
-});
-
-
-
-app.get('/home', function(req, res) {
+app.get('/home', function (req, res) {
     res.render("templates/home");
 });
 
-app.get('/signin', function(req, res) {
-    res.render("templates/signin");
+app.get('/signin', function (req, res) {
+    res.render("templates/signin", { flag: 0 })
+})
+
+app.post('/signin', async function (req, res) {
+    res.render("templates/navpannel", {utype :0 })
 });
 
-app.get('/signup', function(req, res) {
+app.get('/signup', function (req, res) {
     res.render("templates/signup");
 });
 
-app.post('/signup', function(req, res) {
-    console.log(req.body);
-    //DB.signup(req.body);
-    res.redirect("/signin");
+app.get('/about', function (req, res) {
+    res.render('templates/about', { access: 0, submitted: 0 });
 });
 
-app.get('/collagelist',function(req,res){
-    res.render("templates/collagelist")
+app.post('/about', function (req, res) {
+    feedBackData = req.body;
+    DB.feedBack(feedBackData);
+    res.render('templates/about', { access: 0, submitted: 1 });
+
+});
+
+app.get('/studentform',function (req, res){
+    res.render('templates/studentform',{log : logedInUserData, utype : 0}); 
 })
+
+app.get('/studentform2',function (req, res){
+    res.render('templates/studentform2',{log : logedInUserData, utype : 0}); 
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}/`);
-});
+})
